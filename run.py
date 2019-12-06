174e8bb09694c3bcbbe02310d5848f529167bbb5
Doublexe <727801418@qq.com>
2019-12-06 03:12:56 +0000

    fix requirement bug; add essential file detection

--- run.py
+++ run.py
@@ -4,9 +4,10 @@ import os
 
 app = create_app()
 
+if not os.path.exists('./original.yaml'):
+    raise OSError("Please run stage.py first.")
+if not os.path.exists(Config.extracted_path):
+    raise OSError("Please run link_and_clear.py first.")
+
 if __name__ == '__main__':
-    if not os.path.exists('./original.yaml'):
-        raise OSError("Please run stage.py first.")
-    if not os.path.exists(Config.extracted_path):
-        raise OSError("Please run link.py first.")
     app.run(host="0.0.0.0")
